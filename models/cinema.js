f381137cacc6fde897db78d2cf012cda7c3a008e
MattyMcHatty <54686014+MattyMcHatty@users.noreply.github.com>
2022-10-05 11:50:00 +0100

    finished second test

--- models/cinema.js
+++ models/cinema.js
@@ -8,4 +8,7 @@ Cinema.prototype.filmTitles = function(){
   return result = this.films.map((film)=>{ return film.title})
 }
 
+Cinema.prototype.findFilmByTitle = function(film_title){
+  return result = this.films.filter((film)=>{return film_title == film.title})
+}
 module.exports = Cinema;
--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -34,7 +34,13 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, expected);
   });
 
-  it('should be able to find a film by title');
+  it('should be able to find a film by title', function(){
+    const actual = cinema.findFilmByTitle('Dunkirk');
+    const expected = [dunkirk];
+    assert.deepStrictEqual(actual, expected);
+  });
+
+
   it('should be able to filter films by genre');
   it('should be able to check whether there are some films from a particular year');
   it('should be able to check whether there are no films from a particular year');
